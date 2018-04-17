53c89b9d51a32b793c8e884b27d98f043ffb0c3e
Gabriel Wanzek <gw@millenium.de>
2018-04-17 16:25:03 +0200

    Bugfixes for downloader
    - Remove first line (which contained the language)
    - Remove trailing whitespace, commas or dots from the word
    - Fix downloading english words
    - Delete old downloaded files

--- script/index.php
+++ script/index.php
@@ -1,13 +1,19 @@
 <?php
 require 'vendor/autoload.php';
 use Goutte\Client;
+
+shell_exec('rm ' . dirname(__DIR__) . DIRECTORY_SEPARATOR . '*.txt');
+
 $client = new Client();
 $crawler = $client->request('GET', 'http://1000mostcommonwords.com/');
 $crawler->filter('.entry-content p span a')->each(function ($node) {
     $filename = trim(basename($node->attr('href')));
     $client = new Client();
     $crawler_lang = $client->request('GET', $node->attr('href'));
-    $crawler_lang->filter('table td:nth-child(2)')->each(function ($node_lang) use ($filename) {
-        file_put_contents($filename . '.txt', $node_lang->text() . PHP_EOL, FILE_APPEND);
+    $column = (preg_match('/english/', $node->attr('href')) ? 3 : 2);
+    $crawler_lang->filter('table tr:not(:first-child) td:nth-child('.$column.')')->each(function ($node_lang) use ($filename) {
+        $filename = dirname(__DIR__) . DIRECTORY_SEPARATOR . $filename . '.txt';
+        $word = trim($node_lang->text(), ',.') . PHP_EOL;
+        file_put_contents($filename, $word, FILE_APPEND);
     });
 });
